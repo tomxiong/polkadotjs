ed8666d64c96a0791b70dc4c2c916dcbc9a42897
tom xiong <tomxiongzh@gmail.com>
2022-07-31 00:16:53 +0800

    add screen png and fix some bug

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 README.md
 
 * The 6th Homework of learn substract basic
+
+** Subscribe the balance query event of Substrate account
+
+* How to use it 
+```
+yarn install
+yarn start
+```
+At the same time, you must start substrate local node
+```
+./target/debug/node-template --dev --tmp
+```
+Then the front UI console will show the free balance of Alice account like below:
+```
+xionggang@DESKTOP-5HGRQV2:~/node/projects/polkadotjs$ npx ts-node main.ts
+connection to substrate node is ok.
+free balance is 1152921504606846976
+free balance is 1152921504481846796
+```
+The screen shot like below:
+![Screenshot1](screen.png "Subscribe substrate event")
--- main.ts
+++ main.ts
@@ -1,12 +1,43 @@
+import {ApiPromise, Keyring} from '@polkadot/api'
+import {WsProvider} from '@polkadot/rpc-provider';
+import '@polkadot/api-augment'
+
+const WEB_SOCKET = 'ws://localhost:9944';
+const sleep = (ms: number) => new Promise(reslove => setTimeout(reslove, ms));
+
+const connectSubstrate = async() => {
+    const wsProvider = new WsProvider(WEB_SOCKET);
+    const api = await ApiPromise.create({ provider: wsProvider});
+    await api.isReady;
+    console.log("connection to substrate node is ok.");
+    return api;
+};
+
+
+// subscribe balance change
+const subscribeAliceBalance = async (api: ApiPromise) => {
+    const keyring = new Keyring({type: 'sr25519'});
+    const alice = keyring.addFromUri('//Alice');
+    await api.query.system.account(alice.address, aliceAcct => {
+        const freeSub = aliceAcct.data.free
+        console.log(`free balance is ${freeSub}`);
+    });
+};
+
+
 const main = async() => {
-    console.log("hello");
+    const api = await connectSubstrate();
+    await subscribeAliceBalance(api);
+    await sleep(600000);
+
+    console.log("The end of subscribe account balance of Alice.");
 }
 
 main()
 .then(() => {
     console.log("successfully exited");
     process.exit(0);
-    
+
 })
 .catch(err => {
     console.log('error occur:', err);
